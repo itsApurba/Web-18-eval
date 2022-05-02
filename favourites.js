a883cf1b10ea7b33fb6f193c73db71c821919651
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2022-05-02 15:39:14 +0530

    +

--- favourites.js
+++ favourites.js
@@ -20,19 +20,20 @@ favouritesData.forEach((el) => {
   date.innerText = el.date;
   const venue = document.createElement("td");
   venue.innerText = el.venue;
-   const btn = document.createElement("button");
-   btn.innerText = "Favourite";
-   btn.style.backgroundColor = "yellow";
-   btn.style.border = "none";
-   btn.style.padding = "5px";
-   btn.style.borderRadius = "7px";
-   btn.style.cursor = "pointer";
+  const btn = document.createElement("button");
+  btn.innerText = "Remove";
+  btn.style.backgroundColor = "red";
+  btn.style.color = "white";
+  btn.style.border = "none";
+  btn.style.padding = "5px";
+  btn.style.borderRadius = "7px";
+  btn.style.cursor = "pointer";
 
-   btn.addEventListener("click", () => {
-     favouritesData.push(arr[index]);
-     localStorage.setItem("favourites", JSON.stringify(favouritesData));
-   });
-  row.append(matchNum, teamA, teamB, date, venue,btn);
+  btn.addEventListener("click", () => {
+    favouritesData.push(arr[index]);
+    localStorage.setItem("favourites", JSON.stringify(favouritesData));
+  });
+  row.append(matchNum, teamA, teamB, date, venue, btn);
   tbody.append(row);
 
   //   console.log(el.matchNum);
